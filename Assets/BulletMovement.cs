e1080c1c32a163b510d78642ae36be317168c035
Shirini02 <shirini.gone@xcubelabs.com>
2021-06-27 23:53:34 -0700

    ObjectPoolingMethod

---
+++ Assets/BulletMovement.cs
@@ -0,0 +1,32 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMovement : MonoBehaviour
+{
+    public Vector3 velocity;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(velocity);
+    }
+
+    private void OnBecameInvisible()
+    {
+        this.gameObject.SetActive(false);
+        //  Destroy(gameObject);
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "enemy")
+        {
+            collision.gameObject.SetActive(false);
+
+        }
+    }
+
+
+}
